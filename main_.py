0c9bc74501fcba23b459aaceb80b7328b0c2fe76
Joshua Ogundairo <oluwadamilarejoshua01@gmail.com>
2022-09-18 20:23:57 +0100

    Little modification on main_

--- main_.py
+++ main_.py
@@ -1,20 +1,20 @@
-# # import another_module
-#
-# from turtle import Turtle, Screen
-#
-# # print(another_module.another_variable)
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("cyan1")
-# timmy.forward(100)
-# timmy.color("coral")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+# import another_module
+
+from turtle import Turtle, Screen
+
+# print(another_module.another_variable)
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("cyan1")
+timmy.forward(100)
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 from prettytable import PrettyTable
 table = PrettyTable()
